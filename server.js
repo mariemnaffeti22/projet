fc064c6b4d84af1cf426c5c149ec86549fc8d9c8
mariemnaffeti22 <138447520+mariemnaffeti22@users.noreply.github.com>
2023-08-11 15:35:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,115 @@
 const express = require('express');
 const app = express();
-const mysql= require('mysql')
+const mysql = require('mysql');
+const cors = require('cors'); 
 
-const db= mysql.createConnection({
-    user: 'root',
-    host:'localhost',
-    password:'',
-    database:'tachessystem'
-})
-app.post('/create',(req,res)=>{
-const nom= req.body.nom
-const description= req.body.description
-const createur= req.body.createur
-const developpeurs= req.body.developpeurs
-const priorite= req.body.priorite
-const avancement= req.body.avancement
-const dateLimite= req.body.dateLimite
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'systemTaches', 
+});
 
-db.query('INSERT INTO taches(nom,description,createur,developpeurs,priorite,avancement,dateLimite)VALUES (?,?,?,?,?,?,?)',
-[nom,description,createur,developpeurs,priorite,avancement,dateLimite],
-(err,result) =>{
-if (err) {
-    console.log(err)
-}
-else {
-    res.send("tache inserer")
-}
-}
-);
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+  } else {
+    console.log('Connecté à la base de données MySQL !');
+  }
+});
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'application de gestion des tâches');
+});
+
+
+app.post('/create', (req, res) => {
+  const { nom, description, createur, developpeurs, priorite, avancement, dateLimite } = req.body;
+
+  connection.query(
+    'INSERT INTO taches(nom, description, createur, developpeurs, priorite, avancement, dateLimite) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    [nom, description, createur, developpeurs, priorite, avancement, dateLimite],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Erreur lors de l\'insertion de la tâche');
+      } else {
+        res.status(200).send('Tâche insérée avec succès');
+      }
+    }
+  );
+});
+
+
+app.get('/taches', (req, res) => {
+  connection.query('SELECT * FROM taches', (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la récupération des tâches');
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+
+app.get('/taches/:id', (req, res) => {
+  const taskId = req.params.id;
+
+  connection.query('SELECT * FROM taches WHERE id = ?', [taskId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la récupération de la tâche');
+    } else {
+      if (result.length === 0) {
+        res.status(404).send('Tâche non trouvée');
+      } else {
+        res.status(200).json(result[0]);
+      }
+    }
+  });
+});
+
+app.put('/taches/:id', (req, res) => {
+  const taskId = req.params.id;
+  const { nom, description, createur, developpeurs, priorite, avancement, dateLimite } = req.body;
+
+  connection.query(
+    'UPDATE taches SET nom = ?, description = ?, createur = ?, developpeurs = ?, priorite = ?, avancement = ?, dateLimite = ? WHERE id = ?',
+    [nom, description, createur, developpeurs, priorite, avancement, dateLimite, taskId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Erreur lors de la mise à jour de la tâche');
+      } else {
+        res.status(200).send('Tâche mise à jour avec succès');
+      }
+    }
+  );
+});
+
+app.delete('/taches/:id', (req, res) => {
+  const taskId = req.params.id;
+
+  connection.query('DELETE FROM taches WHERE id = ?', [taskId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la suppression de la tâche');
+    } else {
+      res.status(200).send('Tâche supprimée avec succès');
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Votre serveur est en cours d\'exécution sur le port 4000');
+});
+
 
 })
 
